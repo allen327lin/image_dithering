77c2bc8e817531d37fe36a8b5e75c431a8243a36
allen327lin <allen327lin2@gmail.com>
2024-05-26 01:07:32 +0800

    feat: init commit & update README & add utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,4 @@
-# image_dithering
-
-## License
+"""
 MIT License
 
 Copyright (c) 2024 allen327lin
@@ -22,3 +20,40 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+"""
+
+import numpy as np
+import cv2
+
+
+def show_img(title, img, save_img=False):
+    cv2.namedWindow(title, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(title, 400, 400)
+    cv2.imshow(title, img)
+    if save_img:
+        cv2.imwrite("./photos/results/" + title + ".png", img)
+    return 0
+
+
+def show_fft(title, fft, save_img=False):
+    fft = np.log(np.abs(fft) + 1)
+    fft = fft / fft.max() * 255.0
+    fft = np.uint8(fft)
+
+    cv2.namedWindow(title, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(title, 400, 400)
+    cv2.imshow(title, fft)
+    if save_img:
+        cv2.imwrite("./photos/results/" + title + ".png", fft)
+
+    return 0
+
+
+def normalization(arr):
+    min_v = np.min(arr)
+    max_v = np.max(arr)
+
+    arr = (arr - min_v) / (max_v - min_v)
+    arr = (arr * 255).astype(np.uint8)
+
+    return arr
